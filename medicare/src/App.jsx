e6110e1f9c305dae0474c0bfde4898bff5af05d1
rahulbhongade358 <rahulbhongade912@gmail.com>
2025-07-29 20:49:50 +0530

    Routing Setup

--- medicare/src/App.jsx
+++ medicare/src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-
+import Navbar from "./Components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Public/Home";
+import Contact from "./Pages/Public/Contact";
+import Explore from "./Pages/Public/Explore";
+import Login from "./Pages/Public/Login";
+import SignUp from "./Pages/Public/SignUp";
 const App = () => {
-  return <div>hi</div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
--- medicare/src/Components/Navbar.jsx
+++ medicare/src/Components/Navbar.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/signup">Singup</Link>
+    </div>
+  );
 };
 
 export default Navbar;
